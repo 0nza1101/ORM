27b84b5727e3d05d8aaf31c9a2899462d88c2e93
Can Serkan UREN <canserkan.uren@gmail.com>
2017-12-15 20:41:17 +0100

    Changing the structure of the starting project..

--- ORMTest/Program.cs
+++ ORMTest/Program.cs
@@ -4,6 +4,8 @@ using System;
 using System.Collections.Generic;
 using System.Data.Common;
 using System.Data.SqlClient;
+using MySql.Data.MySqlClient;
+using Npgsql;
 
 namespace ORMTest
 {
@@ -32,76 +34,226 @@ namespace ORMTest
 
     class Program
     {
-        static string team = "ME";
+        static string team = "Gabrielle, Jordan, Teddy and Can Serkan";
         static void Main(string[] args)
         {
             Console.WriteLine($"Test cases for ORMLib made by { team }");
+            PostgreSql();
+            MySql();
+            MsSql();
+        }
 
-            DboMapper database = new DboMapper("localhost", "5432", "devdb", "postgres", " ", DatabaseType.PostgreSql);
-          
-           //Simple mapping takes only SELECT
-           string selectReqWithListFunction = "SELECT * FROM contacts";
-           List<Contacts> listMadeByListFunction = database.List<Contacts>(selectReqWithListFunction);
-           foreach (var item in listMadeByListFunction)
-           {
-               Console.WriteLine(item.name, item.address, item.email);
-           }
-           Console.ReadKey();
-           Console.Clear();
+        private static void PostgreSql()
+        {
+            // Creating the database. IF YOU WANT TO TEST IT, DON'T FORGET TO CHANGE THESE PARAMETERS
+            DboMapper database = new DboMapper("localhost", "5432", "devdb", "postgres", "root", DatabaseType.PostgreSql);
 
-           // Calling the Execute Function by giving him a INSERT Statement with parameters
-          string insertReq = @"INSERT INTO contacts (name, address, email) VALUES (@name, @address, @email)";
-           List<DbParameter> pInsert = new List<DbParameter>();
-           pInsert.Add(new SqlParameter("@name", "TEST CASE"));
-           pInsert.Add(new SqlParameter("@address", "RUE DU TEST"));
-           pInsert.Add(new SqlParameter("@email", "TestEmail@gmail.com"));
-           List<Contacts> contactsInsert = database.Execute<Contacts>(insertReq, pInsert);
+            //Simple mapping takes only SELECT
+            string selectReqWithListFunction = "SELECT * FROM contacts";
+            List<Contacts> listMadeByListFunction = database.List<Contacts>(selectReqWithListFunction);
+            foreach (var item in listMadeByListFunction)
+            {
+                Console.WriteLine(item.name, item.address, item.email);
+            }
+            Console.ReadKey();
+            Console.Clear();
 
-           // Calling the Execute Function by giving him a SELECT Statement with parameters
-           string selectReqWithParameters = @"SELECT * FROM contacts WHERE email = @email";
-           List<DbParameter> pSelect = new List<DbParameter>();
-           pSelect.Add(new SqlParameter("@email", "TestEmail@gmail.com"));
-           List<Contacts> contactsSelectWithParameter = database.Execute<Contacts>(selectReqWithParameters, pSelect);
-           foreach (var item in contactsSelectWithParameter)
-           {
-               Console.WriteLine(item.name, item.address, item.email);
-           }
-           Console.ReadKey();
-           Console.Clear();
+            // Calling the Execute Function by giving him a INSERT Statement with parameters
+            string insertReq = @"INSERT INTO contacts (name, address, email) VALUES (@name, @address, @email)";
+            List<DbParameter> pInsert = new List<DbParameter>();
+            pInsert.Add(new NpgsqlParameter("@name", "TEST CASE"));
+            pInsert.Add(new NpgsqlParameter("@address", "RUE DU TEST"));
+            pInsert.Add(new NpgsqlParameter("@email", "TestEmail@gmail.com"));
+            List<Contacts> contactsInsert = database.Execute<Contacts>(insertReq, pInsert);
 
-           // Calling the Execute Function by giving him a UPDATE Statement with parameters
-           string updateReq = @"UPDATE contacts SET name = @name, address = @address, email = @email WHERE email = @oldEmail";
-           List<DbParameter> pUpdate = new List<DbParameter>();
-           pUpdate.Add(new SqlParameter("@name", "NAME CHANGED FOR TEST"));
-           pUpdate.Add(new SqlParameter("@address", "ADDRESS CHANGED FOR TEST"));
-           pUpdate.Add(new SqlParameter("@email", "emailChangedForTest@gmail.com"));
-           pUpdate.Add(new SqlParameter("@oldEmail", "TestEmail@gmail.com"));
-           List<Contacts> contactsUpdate = database.Execute<Contacts>(updateReq, pUpdate);
+            // Calling the Execute Function by giving him a SELECT Statement with parameters
+            string selectReqWithParameters = @"SELECT * FROM contacts WHERE email = @email";
+            List<DbParameter> pSelect = new List<DbParameter>();
+            pSelect.Add(new NpgsqlParameter("@email", "TestEmail@gmail.com"));
+            List<Contacts> contactsSelectWithParameter = database.Execute<Contacts>(selectReqWithParameters, pSelect);
+            foreach (var item in contactsSelectWithParameter)
+            {
+                Console.WriteLine(item.name, item.address, item.email);
+            }
+            Console.ReadKey();
+            Console.Clear();
 
-           // Calling the Execute Function by giving him an SELECT Statement 
-           String selectReq = @"SELECT * FROM CONTACTS";
-           List<Contacts> contactsSelect = database.Execute<Contacts>(selectReq, pSelect);
-           foreach (var item in contactsSelect)
-           {
-               Console.WriteLine(item.name, item.address, item.email);
-           }
-           Console.ReadKey();
-           Console.Clear();
+            // Calling the Execute Function by giving him a UPDATE Statement with parameters
+            string updateReq = @"UPDATE contacts SET name = @name, address = @address, email = @email WHERE email = @oldEmail";
+            List<DbParameter> pUpdate = new List<DbParameter>();
+            pUpdate.Add(new NpgsqlParameter("@name", "NAME CHANGED FOR TEST"));
+            pUpdate.Add(new NpgsqlParameter("@address", "ADDRESS CHANGED FOR TEST"));
+            pUpdate.Add(new NpgsqlParameter("@email", "emailChangedForTest@gmail.com"));
+            pUpdate.Add(new NpgsqlParameter("@oldEmail", "TestEmail@gmail.com"));
+            List<Contacts> contactsUpdate = database.Execute<Contacts>(updateReq, pUpdate);
 
-           // Calling the Execute Function by giving him a DELETE Statement with parameters
-           string deleteReq = @"DELETE FROM contacts WHERE address = @address";
-           List<DbParameter> pDelete = new List<DbParameter>();
-           pDelete.Add(new SqlParameter("@address", "ADDRESS CHANGED FOR TEST"));
-           List<Contacts> contactsDelete = database.Execute<Contacts>(deleteReq, pDelete);
+            // Calling the Execute Function by giving him an SELECT Statement 
+            String selectReq = @"SELECT * FROM CONTACTS";
+            List<Contacts> contactsSelect = database.Execute<Contacts>(selectReq, pSelect);
+            foreach (var item in contactsSelect)
+            {
+                Console.WriteLine(item.name, item.address, item.email);
+            }
+            Console.ReadKey();
+            Console.Clear();
 
-           // Calling the Execute Function by giving him a SELECT Statement
-           contactsSelect = null;
-           contactsSelect = database.Execute<Contacts>(selectReq, pSelect);
-           foreach (var item in contactsSelect)
-           {
-               Console.WriteLine(item.name, item.address, item.email);
-           }
-           Console.ReadKey();
+            // Calling the Execute Function by giving him a DELETE Statement with parameters
+            string deleteReq = @"DELETE FROM contacts WHERE address = @address";
+            List<DbParameter> pDelete = new List<DbParameter>();
+            pDelete.Add(new NpgsqlParameter("@address", "ADDRESS CHANGED FOR TEST"));
+            List<Contacts> contactsDelete = database.Execute<Contacts>(deleteReq, pDelete);
+
+            // Calling the Execute Function by giving him a SELECT Statement
+            contactsSelect = null;
+            contactsSelect = database.Execute<Contacts>(selectReq, pSelect);
+            foreach (var item in contactsSelect)
+            {
+                Console.WriteLine(item.name, item.address, item.email);
+            }
+            Console.WriteLine("Test is finished, you can click on enter to close this tab.");
+            Console.ReadKey();
+        }
+
+        private static void MsSql()
+        {
+            // Creating the database. IF YOU WANT TO TEST IT, DON'T FORGET TO CHANGE THESE PARAMETERS
+            DboMapper database = new DboMapper("localhost", "1433", "devdb", "sa", "P@55w0rd", DatabaseType.MSSql);
+
+            //Simple mapping takes only SELECT
+            string selectReqWithListFunction = "SELECT * FROM contacts";
+            List<Contacts> listMadeByListFunction = database.List<Contacts>(selectReqWithListFunction);
+            foreach (var item in listMadeByListFunction)
+            {
+                Console.WriteLine(item.name, item.address, item.email);
+            }
+            Console.ReadKey();
+            Console.Clear();
+
+            // Calling the Execute Function by giving him a INSERT Statement with parameters
+            string insertReq = @"INSERT INTO contacts (name, address, email) VALUES (@name, @address, @email)";
+            List<DbParameter> pInsert = new List<DbParameter>();
+            pInsert.Add(new SqlParameter("@name", "TEST CASE"));
+            pInsert.Add(new SqlParameter("@address", "RUE DU TEST"));
+            pInsert.Add(new SqlParameter("@email", "TestEmail@gmail.com"));
+            List<Contacts> contactsInsert = database.Execute<Contacts>(insertReq, pInsert);
+
+            // Calling the Execute Function by giving him a SELECT Statement with parameters
+            string selectReqWithParameters = @"SELECT * FROM contacts WHERE email = @email";
+            List<DbParameter> pSelect = new List<DbParameter>();
+            pSelect.Add(new SqlParameter("@email", "TestEmail@gmail.com"));
+            List<Contacts> contactsSelectWithParameter = database.Execute<Contacts>(selectReqWithParameters, pSelect);
+            foreach (var item in contactsSelectWithParameter)
+            {
+                Console.WriteLine(item.name, item.address, item.email);
+            }
+            Console.ReadKey();
+            Console.Clear();
+
+            // Calling the Execute Function by giving him a UPDATE Statement with parameters
+            string updateReq = @"UPDATE contacts SET name = @name, address = @address, email = @email WHERE email = @oldEmail";
+            List<DbParameter> pUpdate = new List<DbParameter>();
+            pUpdate.Add(new SqlParameter("@name", "NAME CHANGED FOR TEST"));
+            pUpdate.Add(new SqlParameter("@address", "ADDRESS CHANGED FOR TEST"));
+            pUpdate.Add(new SqlParameter("@email", "emailChangedForTest@gmail.com"));
+            pUpdate.Add(new SqlParameter("@oldEmail", "TestEmail@gmail.com"));
+            List<Contacts> contactsUpdate = database.Execute<Contacts>(updateReq, pUpdate);
+
+            // Calling the Execute Function by giving him an SELECT Statement 
+            String selectReq = @"SELECT * FROM CONTACTS";
+            List<Contacts> contactsSelect = database.Execute<Contacts>(selectReq, pSelect);
+            foreach (var item in contactsSelect)
+            {
+                Console.WriteLine(item.name, item.address, item.email);
+            }
+            Console.ReadKey();
+            Console.Clear();
+
+            // Calling the Execute Function by giving him a DELETE Statement with parameters
+            string deleteReq = @"DELETE FROM contacts WHERE address = @address";
+            List<DbParameter> pDelete = new List<DbParameter>();
+            pDelete.Add(new SqlParameter("@address", "ADDRESS CHANGED FOR TEST"));
+            List<Contacts> contactsDelete = database.Execute<Contacts>(deleteReq, pDelete);
+
+            // Calling the Execute Function by giving him a SELECT Statement
+            contactsSelect = null;
+            contactsSelect = database.Execute<Contacts>(selectReq, pSelect);
+            foreach (var item in contactsSelect)
+            {
+                Console.WriteLine(item.name, item.address, item.email);
+            }
+            Console.WriteLine("Test is finished, you can click on enter to close this tab.");
+            Console.ReadKey();
+        }
+
+        private static void MySql()
+        {
+            // Creating the database. IF YOU WANT TO TEST IT, DON'T FORGET TO CHANGE THESE PARAMETERS
+            DboMapper database = new DboMapper("localhost", "", "devdb", "root", "root", DatabaseType.MySql);
+
+            //Simple mapping takes only SELECT
+            string selectReqWithListFunction = "SELECT * FROM contacts";
+            List<Contacts> listMadeByListFunction = database.List<Contacts>(selectReqWithListFunction);
+            foreach (var item in listMadeByListFunction)
+            {
+                Console.WriteLine(item.name, item.address, item.email);
+            }
+            Console.ReadKey();
+            Console.Clear();
+
+            // Calling the Execute Function by giving him a INSERT Statement with parameters
+            string insertReq = @"INSERT INTO contacts (name, address, email) VALUES (@name, @address, @email)";
+            List<DbParameter> pInsert = new List<DbParameter>();
+            pInsert.Add(new MySqlParameter("@name", "TEST CASE"));
+            pInsert.Add(new MySqlParameter("@address", "RUE DU TEST"));
+            pInsert.Add(new MySqlParameter("@email", "TestEmail@gmail.com"));
+            List<Contacts> contactsInsert = database.Execute<Contacts>(insertReq, pInsert);
+
+            // Calling the Execute Function by giving him a SELECT Statement with parameters
+            string selectReqWithParameters = @"SELECT * FROM contacts WHERE email = @email";
+            List<DbParameter> pSelect = new List<DbParameter>();
+            pSelect.Add(new MySqlParameter("@email", "TestEmail@gmail.com"));
+            List<Contacts> contactsSelectWithParameter = database.Execute<Contacts>(selectReqWithParameters, pSelect);
+            foreach (var item in contactsSelectWithParameter)
+            {
+                Console.WriteLine(item.name, item.address, item.email);
+            }
+            Console.ReadKey();
+            Console.Clear();
+
+            // Calling the Execute Function by giving him a UPDATE Statement with parameters
+            string updateReq = @"UPDATE contacts SET name = @name, address = @address, email = @email WHERE email = @oldEmail";
+            List<DbParameter> pUpdate = new List<DbParameter>();
+            pUpdate.Add(new MySqlParameter("@name", "NAME CHANGED FOR TEST"));
+            pUpdate.Add(new MySqlParameter("@address", "ADDRESS CHANGED FOR TEST"));
+            pUpdate.Add(new MySqlParameter("@email", "emailChangedForTest@gmail.com"));
+            pUpdate.Add(new MySqlParameter("@oldEmail", "TestEmail@gmail.com"));
+            List<Contacts> contactsUpdate = database.Execute<Contacts>(updateReq, pUpdate);
+
+            // Calling the Execute Function by giving him an SELECT Statement 
+            String selectReq = @"SELECT * FROM CONTACTS";
+            List<Contacts> contactsSelect = database.Execute<Contacts>(selectReq, pSelect);
+            foreach (var item in contactsSelect)
+            {
+                Console.WriteLine(item.name, item.address, item.email);
+            }
+            Console.ReadKey();
+            Console.Clear();
+
+            // Calling the Execute Function by giving him a DELETE Statement with parameters
+            string deleteReq = @"DELETE FROM contacts WHERE address = @address";
+            List<DbParameter> pDelete = new List<DbParameter>();
+            pDelete.Add(new MySqlParameter("@address", "ADDRESS CHANGED FOR TEST"));
+            List<Contacts> contactsDelete = database.Execute<Contacts>(deleteReq, pDelete);
+
+            // Calling the Execute Function by giving him a SELECT Statement
+            contactsSelect = null;
+            contactsSelect = database.Execute<Contacts>(selectReq, pSelect);
+            foreach (var item in contactsSelect)
+            {
+                Console.WriteLine(item.name, item.address, item.email);
+            }
+            Console.WriteLine("Test is finished, you can click on enter to close this tab.");
+            Console.ReadKey();
         }
     }
 }
